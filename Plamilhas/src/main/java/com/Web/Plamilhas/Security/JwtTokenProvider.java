717a9dafbc6b1a565361ec108ff74a4ceb38f496
Andre de Padua <andredepadua2011@gmail.com>
2025-11-04 13:33:01 -0300

    Retirada de erros classe Security

--- Plamilhas/src/main/java/com/Web/Plamilhas/Security/JwtTokenProvider.java
+++ Plamilhas/src/main/java/com/Web/Plamilhas/Security/JwtTokenProvider.java
@@ -1,25 +1,23 @@
 package com.Web.Plamilhas.Security;
 
 import java.security.Key;
-import java.security.Signature;
-import java.sql.Date;
-
+import java.util.Date;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.powermock.tests.utils.Keys;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
 
     private static final String SECRET = "umasecretkeyseguraegrandeparajwtplaMilhas2025";
-    private static final long EXPIRATION = 1000*60*60*24;
+    private static final long EXPIRATION = 1000 * 60 * 60 * 24;
 
+    
     private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
     
-    public String gerarToken (String email){
+     public String gerarToken (String email){
         Date agora = new Date();
         Date validade = new Date(agora.getTime() + EXPIRATION);
         return Jwts.builder()
@@ -29,12 +27,17 @@ public class JwtTokenProvider {
                 .signWith(key,SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    
     public String obterEmailDoToken(String token) {
-        return Jwts.parserBuilder().verifyWith(key).build()
+        
+        return Jwts.parserBuilder().setSigningKey(key).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
+    
     public boolean validarToken(String token){
         try{
+            
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e){
